48355e71de34bf498c05d489bcfa60a5603851fc
shvarsha <130674453+shvarsha@users.noreply.github.com>
2023-07-26 16:22:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource null_resource "test" {
     provisioner "local-exec" {
     command = <<EOT
       curl google.com
+      curl -s 'https://api.github.com/users/lambda' | jq -r '.name'
     EOT
     }
   
