cec6520224e4ec7ee936915c9c597ec658d6f2be
shvarsha <130674453+shvarsha@users.noreply.github.com>
2023-07-26 19:07:03 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 ibmcloud_api_key_demo=$1
 url=$2
 
-cmd="curl -X POST 'https://iam.cloud.ibm.com/identity/token' -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${ibmcloud_api_key_demo}"
+cmd="curl -X POST "https://iam.cloud.ibm.com/identity/token" -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${ibmcloud_api_key_demo}"
 result=`$cmd | jq -r ".access_token"`
 curl -H "Authorization: Bearer $bearer_token" -X POST "http://${url}/test-database" -H "Content-Type: application/json" 
 --data '{ 
