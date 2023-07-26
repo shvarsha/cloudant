8401e1bfc8cb1d3f47068be8afbc2f6ed12fab93
shvarsha <130674453+shvarsha@users.noreply.github.com>
2023-07-26 18:45:16 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+ibmcloud_api_key_demo=$1
+url=$2
+echo $url
+echo $ibmcloud_api_key_demo
+cmd="curl -X POST 'https://iam.cloud.ibm.com/identity/token' -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${ibmcloud_api_key_demo}"
+result=`$cmd | jq -r ".access_token"`
+echo "#####################"
+echo $bearer_token
+`curl -H "Authorization: Bearer $bearer_token" -X POST "${url}/test-database" -H "Content-Type: application/json" 
+--data '{ 
+    "_id": "001", 
+    "order_id": "order1234",
+    "satelite_id": "sal123456"
+ }`
