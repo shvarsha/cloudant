1f0a59fbb724c03574929320e6c34547ed3fe4d6
shvarsha <130674453+shvarsha@users.noreply.github.com>
2023-07-26 21:40:31 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-
 ibmcloud_api_key_demo=$1
+
 url=$2
 
-cmd="curl -X POST "https://iam.cloud.ibm.com/identity/token" -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${ibmcloud_api_key_demo}'"
-result=`$cmd | jq -r ".access_token"`
-`curl -H "Authorization: Bearer $bearer_token" -X POST "https://${url}/test-database" -H "Content-Type: application/json" 
---data '{ 
-    "_id": "001", 
+echo $url
+
+echo $ibmcloud_api_key_demo
+
+
+bearer_token=$(curl -X POST "https://iam.cloud.ibm.com/identity/token" -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${ibmcloud_api_key_demo}" | jq -r ".access_token")
+
+echo "#####################"
+
+curl -X GET --location "${url}//test-database" \
+--header 'Content-Type: application/json' \
+--header "Authorization: Bearer ${bearer_token}" \
+--data '{
+    "_id": "001",
     "order_id": "order1234",
     "satelite_id": "sal123456"
- }'`
+ }'
